dc85df2d70b40a5de5a3b6100d1c5fa2e00c163c
MHAE391 <modatherelgohary391@gmail.com>
2023-05-23 02:00:51 +0300

    UI

---
+++ android/app/src/main/kotlin/com/m391/planet/planet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.m391.planet.planet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/screens/profile_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:planet/ui/screens/widgets/profile_widget.dart';
+
+import '../../constants.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Constants.primaryColor.withOpacity(.5),
+                  width: 5.0,
+                ),
+              ),
+              child: const CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: size.width * .3,
+              child: Row(
+                children: [
+                  Text(
+                    'Test Test',
+                    style: TextStyle(
+                      color: Constants.blackColor,
+                      fontSize: 20,
+                      fontFamily: AutofillHints.name
+                    ),
+                  ),
+                  SizedBox(
+                      height: 24,
+                      child: Image.asset("assets/images/verified.png")),
+                ],
+              ),
+            ),
+            Text(
+              'test.test@gmail.com',
+              style: TextStyle(
+                color: Constants.blackColor.withOpacity(.3),
+                fontSize: 18
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: size.height * .7,
+              width: size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  ProfileWidget(
+                    icon: Icons.person,
+                    title: 'My Profile',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.settings,
+                    title: 'Settings',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.notifications,
+                    title: 'Notifications',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.chat,
+                    title: 'FAQs',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.share,
+                    title: 'Share',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.logout,
+                    title: 'Log Out',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
